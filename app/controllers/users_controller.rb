fe6354c9947b17ee9d3929794740a8f2223c13eb
Anna Baburina <4nna.baburina@gmail.com>
2022-05-11 21:27:56 +0500

    ADD Successful profile creation notice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to new_user_path, notice: "Thank you. Your profile has been created."
     else
       render :new, status: :unprocessable_entity
     end
